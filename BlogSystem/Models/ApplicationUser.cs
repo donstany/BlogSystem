80cc812ae2d8bb973d1c99ad426123c338ce5b87
donstany <donstany79@gmail.com>
2017-05-07 23:44:45 +0300

    Extract Identity class and DbContext Class

---
+++ BlogSystem/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿namespace BlogSystem.Models
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+
+}
---
+++ BlogSystem/Models/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace BlogSystem.Models
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class BlogDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public BlogDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static BlogDbContext Create()
+        {
+            return new BlogDbContext();
+        }
+    }
+}
